b43779c32a1d9d19e00451903a312c139bcbbf50
Brian Mutinda <mutindabrian26@gmail.com>
2022-06-11 08:28:29 +0300

    add packages

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,12 +11,42 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: Container(
-        child: const Text("Splash Screen"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Login"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 8,
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey[300],
+                child: const Center(
+                    child: Text(
+                  "IMAGE",
+                  style: TextStyle(fontSize: 18),
+                )),
+              ),
+            ),
+            Expanded(
+                flex: 2,
+                child: Center(
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 50,
+                    color: Colors.yellow[500],
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/mainMenu');
+                    },
+                    child: const Text(
+                      "Play",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ))
+          ],
+        ));
   }
 }
